ad5de5492a6642f03895795d3624d007d9cce0e0
Sonika21Rastogi <Snkrstg@gmail.com>
2024-06-26 14:36:02 +0530

    new line changes

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, Worldsss!\n');
+  res.end('Hello,sonika!\n');
 });
 
 server.listen(port, hostname, () => {
